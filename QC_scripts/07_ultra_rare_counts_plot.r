530242a35aa93d39f3283e281aa77409f31c60b5
astheeggeggs <youremail@email.com>
2021-10-10 17:40:00 +0100

    updated plotting

--- QC_scripts/07_ultra_rare_counts_plot.r
+++ QC_scripts/07_ultra_rare_counts_plot.r
@@ -8,6 +8,7 @@ library(data.table)
 source('utils/pretty_plotting.r')
 # Thresholds and plotting file locations defined in r_options.r
 source("utils/r_options.r")
+source("utils/helpers.r")
 
 suppressPackageStartupMessages(library("argparse"))
 
@@ -32,6 +33,7 @@ setkey(dt, 's')
 
 for (CHR in seq(2,22)) {
     # Input files
+    cat(paste0("chromosome ", CHR, "\n"))
     URV_FILE <- paste0('/well/lindgren/UKBIOBANK/dpalmer/wes_', TRANCHE,
 	'/ukb_wes_qc/data/samples/07_URVs_chr',  CHR, '.tsv.bgz')
 
@@ -63,15 +65,15 @@ system(paste("bgzip", URV_COMBINED_FILE))
 dt <- dt[sample(nrow(dt), replace=FALSE),]
 
 # Scatters of URV-SNPs against URV-Indels.
-p <- ggplot(dt, aes(x=n_coding_URV_SNP, y=n_coding_URV_indel, colour=factor(genetic.eur.no.fin.oct2021))) + 
-geom_point(size=0.5, alpha=0.5) + 
+p <- ggplot(dt, aes(x=n_coding_URV_SNP, y=n_coding_URV_indel, colour=factor(genetic_eur_no_fin_oct2021))) + 
+geom_point_rast(size=0.5, alpha=0.5) + 
 scale_color_d3('category10') + theme_minimal() + labs(x='n URV SNPs', y='n URV indels', color='')
 p <- ggExtra::ggMarginal(p, type = "density",
   xparams = list(adjust=1), yparams=list(adjust=1.5))
 ggsave(paste0(PLOTS, '07_URVs_SNPs_vs_indels_by_NFE.pdf'), p, width=160, height=90, units='mm')
 
-p <- ggplot(dt, aes(x=n_coding_URV_SNP, y=n_coding_URV_indel, colour=factor(white.british))) + 
-geom_point(size=0.5, alpha=0.5) + 
+p <- ggplot(dt, aes(x=n_coding_URV_SNP, y=n_coding_URV_indel, colour=factor(white_british))) + 
+geom_point_rast(size=0.5, alpha=0.5) + 
 scale_color_d3('category10') + theme_minimal() + labs(x='n URV SNPs', y='n URV indels', color='')
 p <- ggExtra::ggMarginal(p, type = "density",
   xparams = list(adjust=1), yparams=list(adjust=1.5))
@@ -85,15 +87,15 @@ titles <- c(
 	'Number of singletons, split by NFE status'
     )
 
-create_pretty_boxplots(dt, aes(x=factor(ukbb.centre), y=(n_coding_URV_SNP + n_coding_URV_indel)), aes(colour=factor(genetic.eur.no.fin.oct2021)), 
+create_pretty_boxplots(dt %>% filter(!is.na(ukbb_centre)), aes(x=factor(ukbb_centre), y=(n_coding_URV_SNP + n_coding_URV_indel)), aes(colour=factor(genetic_eur_no_fin_oct2021)), 
 	y_label=y_label_batch[1], x_label='Number of Singletons', key_label='',
-	title=titles[1], legend=TRUE, save_figure=save_figures,  file=paste0(PLOTS,'07_URVs_by_centre'), xlim=c(0,10),
+	title=titles[1], legend=TRUE, save_figure=save_figures,  file=paste0(PLOTS,'07_URVs_by_centre'), xlim=c(0,NA),
 	threshold=T_nURVSNP)
-create_pretty_boxplots(dt, aes(x=factor(self.report.ethnicity), y=(n_coding_URV_SNP + n_coding_URV_indel)), aes(colour=factor(genetic.eur.no.fin.oct2021)), 
+create_pretty_boxplots(dt %>% filter(!is.na(self_report_ethnicity)), aes(x=factor(self_report_ethnicity), y=(n_coding_URV_SNP + n_coding_URV_indel)), aes(colour=factor(genetic_eur_no_fin_oct2021)), 
     y_label=y_label_batch[1], x_label='Number of Singletons', key_label='',
-    title=titles[2], legend=TRUE, save_figure=save_figures,  file=paste0(PLOTS,'07_URVs_by_ethnicity'), xlim=c(0,10),
+    title=titles[2], legend=TRUE, save_figure=save_figures,  file=paste0(PLOTS,'07_URVs_by_ethnicity'), xlim=c(0,NA),
     threshold=T_nURVSNP)
-create_pretty_boxplots(dt, aes(x=factor(genetic.eur.no.fin.oct2021), y=(n_coding_URV_SNP + n_coding_URV_indel)), aes(colour=factor(ukbb.centre)),
+create_pretty_boxplots(dt %>% filter(!is.na(genetic_eur_no_fin_oct2021)), aes(x=factor(genetic_eur_no_fin_oct2021), y=(n_coding_URV_SNP + n_coding_URV_indel)), aes(colour=factor(ukbb_centre)),
 	y_label=y_label_batch[2], x_label='Number of Singletons', key_label='UKBB centre',
-	title=titles[3], legend=TRUE, save_figure=save_figures,  file=paste0(PLOTS,'07_URVs_by_batch'),  xlim=c(0,10),
+	title=titles[3], legend=TRUE, save_figure=save_figures,  file=paste0(PLOTS,'07_URVs_by_batch'),  xlim=c(0,NA),
 	threshold=T_nURVSNP)
