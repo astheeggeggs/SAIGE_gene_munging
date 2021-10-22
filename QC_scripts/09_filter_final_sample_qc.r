29526afb37188271060a8ecb7a752ac71905fdd7
astheeggeggs <youremail@email.com>
2021-10-22 18:29:47 +0100

    updated website and QC summary

--- QC_scripts/09_filter_final_sample_qc.r
+++ QC_scripts/09_filter_final_sample_qc.r
@@ -40,7 +40,7 @@ dt_keep_ti_tv <- group_by(dt_after, sequencing_batch) %>%
     is.na(sd)
     )
 
-dt_keep <- dt_keep_ti_tv %>% inner_join(dt_keep, by='s')
+dt_keep <- dt_keep_ti_tv %>% inner_join(dt_keep, by='s') %>% select(s)
 print(paste0("Remove Ti/Tv outliers: ", nrow(dt_keep), " samples remain"))
 
 # r_het_hom_var
@@ -53,7 +53,7 @@ dt_keep_het_hom_var <- group_by(dt_after, sequencing_batch) %>%
     is.na(sd)
     )
 
-dt_keep <- dt_keep_het_hom_var %>% inner_join(dt_keep, by='s')
+dt_keep <- dt_keep_het_hom_var %>% inner_join(dt_keep, by='s') %>% select(s)
 print(paste0("Remove Het/HomVar outliers: ", nrow(dt_keep), " samples remain"))
 
 # r_insertion_deletion
@@ -66,34 +66,50 @@ dt_keep_insertion_deletion <- group_by(dt_after, sequencing_batch) %>%
     is.na(sd)
     )
 
-dt_keep <- dt_keep_insertion_deletion %>% inner_join(dt_keep, by='s')
+dt_keep <- dt_keep_insertion_deletion %>% inner_join(dt_keep, by='s') %>% select(s)
 print(paste0("Remove Ins/Del outliers: ", nrow(dt_keep), " samples remain"))
 
 # n_singletons
 T_n_singletons <- 175
 dt_keep_n_singletons <- dt_after %>% filter((sample_qc.n_singleton <= T_n_singletons) | is.na(sample_qc.n_singleton))
-dt_keep <- dt_keep_n_singletons %>% inner_join(dt_keep, by='s')
+dt_keep <- dt_keep_n_singletons %>% inner_join(dt_keep, by='s') %>% select(s)
 
 print(paste0("Remove n_singletons outliers: ", nrow(dt_keep), " samples remain"))
 
-dt_final_sample_summary <- data.table(
-  Filter = c(
-    "Samples after population filters",
-    paste0("Within batch Ti/Tv ratio outside ", n_SDs, " standard deviations"),
-    paste0("Within batch Het/HomVar ratio outside ", n_SDs, " standard deviations"),
-    paste0("Within batch Insertion/Deletion ratio outside ", n_SDs, " standard deviations"),
-    paste0("n singletons >", T_n_singletons),
-    "Samples after final sample filters"),
-  Samples = c(
-    nrow(dt_after),
-    nrow(dt_after) - nrow(dt_keep_ti_tv),
-    nrow(dt_after) - nrow(dt_keep_het_hom_var),
-    nrow(dt_after) - nrow(dt_keep_insertion_deletion),
-    nrow(dt_after) - nrow(dt_keep_n_singletons),
-    nrow(dt_keep))
+summary_fun <- function(dt) {
+  return(
+    dt %>% summarize(
+        "Samples"=sum(!is.na(sequencing_batch)),
+        "Batch 1" = sum(sequencing_batch=="Batch 1", na.rm=TRUE),
+        "Batch 2" = sum(sequencing_batch=="Batch 2", na.rm=TRUE)
+        )
+    )
+}
+
+dt_after_summary <- summary_fun(dt_after)
+dt_final_sample_summary <- rbind(
+  summary_fun(dt_after),
+  dt_after_summary - summary_fun(dt_keep_ti_tv),
+  dt_after_summary - summary_fun(dt_keep_het_hom_var),
+  dt_after_summary - summary_fun(dt_keep_insertion_deletion),
+  dt_after_summary - summary_fun(dt_keep_n_singletons),
+  summary_fun(merge(dt_keep, dt_after))
   )
 
-fwrite(dt_final_sample_summary, file=FINAL_SAMPLE_SUMMARY, quote=FALSE, row.names=FALSE, col.names=FALSE, sep='\t')
+dt_final_sample_summary <- cbind(
+  data.table(
+    Filter = c(
+      "Samples after population filters",
+      paste0("Within batch Ti/Tv ratio outside ", n_SDs, " standard deviations"),
+      paste0("Within batch Het/HomVar ratio outside ", n_SDs, " standard deviations"),
+      paste0("Within batch Insertion/Deletion ratio outside ", n_SDs, " standard deviations"),
+      paste0("n singletons > ", T_n_singletons),
+      "Samples after final sample filters"
+      )
+    ),
+  dt_final_sample_summary)
+
+fwrite(dt_final_sample_summary, file=FINAL_SAMPLE_SUMMARY, quote=FALSE, row.names=FALSE, col.names=TRUE, sep='\t')
 
 # write out
 fwrite(dt_keep, file=FINAL_SAMPLE_LIST, quote=FALSE, row.names=FALSE, col.names=FALSE)
