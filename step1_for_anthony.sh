70cb83926e3178d0b85e202cd750cb2c8070f47e
astheeggeggs <youremail@email.com>
2022-05-08 11:58:39 +0100

    updated files for anthony

--- step1_for_anthony.sh
+++ step1_for_anthony.sh
@@ -4,7 +4,8 @@ saige_singularity_dir="/well/lindgren/dpalmer/saige_singularity"
 version="1.0.5"
 singularity shell --bind /well/lindgren/UKBIOBANK,/well/lindgren/dpalmer ${saige_singularity_dir}/saige.${version}.sif
 
-# Navigate to the extdata folder and run this:
+# Navigate to the extdata folder of the new version of SAIGE (https://github.com/saigegit/SAIGE) and run this:
+cd /well/lindgren/dpalmer/SAIGE/extdata
 
 phenofile="/well/lindgren/UKBIOBANK/dpalmer/ukb_wes_phenotypes/200k/UKBB_WES200k_filtered_cts_dec2021_phenotypes.tsv.gz"
 phenotype="Alanine_aminotransferase_residual"
@@ -15,20 +16,60 @@ trait_type="quantitative"
 plinkFile="/well/lindgren/UKBIOBANK/dpalmer/ukb_genotype_plink/ukb_snp_1_22_including_rare_saige_input"
 outputPrefix="/well/lindgren/UKBIOBANK/dpalmer/ukb_wes_SAIGE_output/200k/${phenotype}"
 outputPrefix_varRatio="/well/lindgren/UKBIOBANK/dpalmer/ukb_wes_SAIGE_output/200k/${phenotype}_cate"
-IsSparseKin="TRUE"
 sparse_GRMFile="/well/lindgren/UKBIOBANK/flassen/projects/KO/wes_ko_ukbb/data/saige/grm/input/211101_long_ukb_wes_200k_sparse_autosomes_relatednessCutoff_0.125_1000_randomMarkersUsed.sparseGRM.mtx"
 sparse_GRM_SampleID="/well/lindgren/UKBIOBANK/flassen/projects/KO/wes_ko_ukbb/data/saige/grm/input/211101_long_ukb_wes_200k_sparse_autosomes_relatednessCutoff_0.125_1000_randomMarkersUsed.sparseGRM.mtx.sampleIDs.txt"
 
 Rscript step1_fitNULLGLMM.R \
     --plinkFile ${plinkFile} \
-    --phenoFile ${phenofile} --phenoCol ${phenotype} \
-    --covarColList ${covars} --sampleIDColinphenoFile ${ID_col} \
+    --phenoFile ${phenofile} \
+    --phenoCol ${phenotype} \
+    --covarColList ${covars} \
+    --sampleIDColinphenoFile ${ID_col} \
     --traitType ${trait_type} \
     --invNormalize TRUE \
     --LOCO FALSE \
     --outputPrefix ${outputPrefix} --outputPrefix_varRatio ${outputPrefix_varRatio} \
     --IsOverwriteVarianceRatioFile TRUE \
     --sparseGRMFile ${sparse_GRMFile} --sparseGRMSampleIDFile ${sparse_GRM_SampleID} \
+    --isCateVarianceRatio TRUE \
+    --cateVarRatioMinMACVecExclude "0.5,1.5,2.5,3.5,4.5,5.5,10.5,20.5" \
+    --cateVarRatioMaxMACVecInclude "1.5,2.5,3.5,4.5,5.5,10.5,20.5" \
+    --useSparseGRMtoFitNULL TRUE 
+
+# This is an example of an interactive singularity container for step 1 - using the old version of SAIGE
+
+saige_singularity_dir="/well/lindgren/dpalmer/saige_singularity"
+version="0.45"
+singularity shell --bind /well/lindgren/UKBIOBANK,/well/lindgren/dpalmer ${saige_singularity_dir}/saige.${version}.sif
+
+# Navigate to the extdata folder of the old version of SAIGE (https://github.com/weizhouUMICH/SAIGE) and run this:
+cd /well/lindgren/dpalmer/SAIGE_old/extdata
+
+phenofile="/well/lindgren/UKBIOBANK/dpalmer/ukb_wes_phenotypes/200k/UKBB_WES200k_filtered_cts_dec2021_phenotypes.tsv.gz"
+phenotype="Alanine_aminotransferase_residual"
+covars="age,PC1,PC2,PC3,PC4,PC5,PC6,PC7,PC8,PC9,PC10,sex,sequencing.batch"
+ID_col="eid"
+trait_type="quantitative"
+
+plinkFile="/well/lindgren/UKBIOBANK/dpalmer/ukb_genotype_plink/ukb_snp_1_22_including_rare_saige_input"
+outputPrefix="/well/lindgren/UKBIOBANK/dpalmer/ukb_wes_SAIGE_output/200k/${phenotype}"
+outputPrefix_varRatio="/well/lindgren/UKBIOBANK/dpalmer/ukb_wes_SAIGE_output/200k/${phenotype}_cate"
+sparse_GRMFile="/well/lindgren/UKBIOBANK/flassen/projects/KO/wes_ko_ukbb/data/saige/grm/input/211101_long_ukb_wes_200k_sparse_autosomes_relatednessCutoff_0.125_1000_randomMarkersUsed.sparseGRM.mtx"
+sparse_GRM_SampleID="/well/lindgren/UKBIOBANK/flassen/projects/KO/wes_ko_ukbb/data/saige/grm/input/211101_long_ukb_wes_200k_sparse_autosomes_relatednessCutoff_0.125_1000_randomMarkersUsed.sparseGRM.mtx.sampleIDs.txt"
+
+Rscript step1_fitNULLGLMM.R \
+    --plinkFile ${plinkFile} \
+    --phenoFile ${phenofile} \
+    --phenoCol ${phenotype} \
+    --covarColList ${covars} \
+    --sampleIDColinphenoFile ${ID_col} \
+    --traitType ${trait_type} \
+    --invNormalize TRUE \
+    --LOCO FALSE \
+    --outputPrefix ${outputPrefix} --outputPrefix_varRatio ${outputPrefix_varRatio} \
+    --IsOverwriteVarianceRatioFile TRUE \
+    --sparseGRMFile ${sparse_GRMFile} \
+    --sparseGRMSampleIDFile ${sparse_GRM_SampleID} \
     --isCateVarianceRatio TRUE \
     --cateVarRatioMinMACVecExclude "0.5,1.5,2.5,3.5,4.5,5.5,10.5,20.5" \
     --cateVarRatioMaxMACVecInclude "1.5,2.5,3.5,4.5,5.5,10.5,20.5" \
--- step2_for_anthony.sh
+++ step2_for_anthony.sh
@@ -1,16 +1,19 @@
 # This is an example of an interactive singularity container for step 2.
 
 saige_singularity_dir="/well/lindgren/dpalmer/saige_singularity"
-version="1.0.5"
+version="1.0.7"
 singularity shell --bind /well/lindgren/UKBIOBANK,/well/lindgren/dpalmer ${saige_singularity_dir}/saige.${version}.sif
 
-# Navigate to the extdata folder and run this:
+# Navigate to the extdata folder of the new version of SAIGE (https://github.com/saigegit/SAIGE) and run this:
+cd /well/lindgren/dpalmer/SAIGE/extdata
 
 chr="20"
 vcfFile="/well/lindgren/UKBIOBANK/dpalmer/wes_200k/ukb_wes_qc/data/final_mt/10_european.strict_filtered_chr${chr}.vcf.bgz"
 vcfFileIndex="/well/lindgren/UKBIOBANK/dpalmer/wes_200k/ukb_wes_qc/data/final_mt/10_european.strict_filtered_chr${chr}.vcf.bgz.csi"
 annotation="pLoF"
 groupFile="/well/lindgren/UKBIOBANK/dpalmer/ukb_wes_variants_vep/200k/SAIGE_gene_input/ukb_wes_200k_filtered_chr${chr}_${annotation}_saige_gene.tsv.gz"
+# I created the groupFile below using the helper function.
+groupFile="/well/lindgren/UKBIOBANK/dpalmer/ukb_wes_variants_vep/200k/SAIGE_gene_input/tmp_groupFile.tsv"
 phenotypeFolder="/well/lindgren/UKBIOBANK/dpalmer/ukb_wes_SAIGE_output/200k"
 phenotype="Alanine_aminotransferase_residual"
 GMMATmodelFile="${phenotypeFolder}/${phenotype}.rda"
@@ -19,20 +22,66 @@ sparse_GRMFile="/well/lindgren/UKBIOBANK/flassen/projects/KO/wes_ko_ukbb/data/sa
 sparse_GRM_SampleID="/well/lindgren/UKBIOBANK/flassen/projects/KO/wes_ko_ukbb/data/saige/grm/input/211101_long_ukb_wes_200k_sparse_autosomes_relatednessCutoff_0.125_1000_randomMarkersUsed.sparseGRM.mtx.sampleIDs.txt"
 outputfile="/well/lindgren/UKBIOBANK/dpalmer/${phenotype}_chr${chr}_${annotation}_test"
 
+Rscript create_tmp_groupFile.R --groupFile
+
 Rscript step2_SPAtests.R \
     --vcfFile ${vcfFile} \
     --vcfField GT \
     --vcfFileIndex ${vcfFileIndex} \
     --groupFile ${groupFile} \
-    --chr "chr${chr}" \
-    --minMAF 0 --maxMAF_in_groupTest 0.01 \
+    --minMAF 0 \
+    --maxMAF_in_groupTest 0.01 \
     --GMMATmodelFile ${GMMATmodelFile} \
     --varianceRatioFile ${varianceRatioFile} \
-    --sparseGRMFile ${sparse_GRMFile} --sparseGRMSampleIDFile ${sparse_GRM_SampleID} \
+    --sparseGRMFile ${sparse_GRMFile} \
+    --sparseGRMSampleIDFile ${sparse_GRM_SampleID} \
     --is_output_moreDetails TRUE \
     --LOCO FALSE \
-    --groupFile ${groupFile} \
     --is_single_in_groupTest TRUE \
     --cateVarRatioMinMACVecExclude "0.5,1.5,2.5,3.5,4.5,5.5,10.5,20.5" \
     --cateVarRatioMaxMACVecInclude "1.5,2.5,3.5,4.5,5.5,10.5,20.5" \
-    --SAIGEOutputFile ${outputfile}
+    --SAIGEOutputFile ${outputfile} \
+    --annotation_in_groupTest="pLoF"
+
+
+saige_singularity_dir="/well/lindgren/dpalmer/saige_singularity"
+version="0.45"
+singularity shell --bind /well/lindgren/UKBIOBANK,/well/lindgren/dpalmer ${saige_singularity_dir}/saige.${version}.sif
+
+# Navigate to the extdata folder of the old version of SAIGE (https://github.com/weizhouUMICH/SAIGE) and run this:
+cd /well/lindgren/dpalmer/SAIGE_old/extdata
+
+chr="20"
+annotation="pLoF"
+vcfFile="/well/lindgren/UKBIOBANK/dpalmer/wes_200k/ukb_wes_qc/data/final_mt/10_european.strict_filtered_chr${chr}.vcf.bgz"
+vcfFileIndex="/well/lindgren/UKBIOBANK/dpalmer/wes_200k/ukb_wes_qc/data/final_mt/10_european.strict_filtered_chr${chr}.vcf.bgz.csi"
+groupFile="/well/lindgren/UKBIOBANK/dpalmer/ukb_wes_variants_vep/200k/SAIGE_gene_input/ukb_wes_200k_filtered_chr${chr}_${annotation}_saige_gene.tsv.gz"
+phenotypeFolder="/well/lindgren/UKBIOBANK/dpalmer/ukb_wes_SAIGE_output/200k"
+phenotype="Alanine_aminotransferase_residual"
+GMMATmodelFile="${phenotypeFolder}/${phenotype}.rda"
+varianceRatioFile="${phenotypeFolder}/${phenotype}_cate.varianceRatio.txt"
+sparseSigmaFile="${phenotypeFolder}/${phenotype}_cate.varianceRatio.txt_relatednessCutoff_0.125_1000_randomMarkersUsed.sparseSigma.mtx"
+outputfile="/well/lindgren/UKBIOBANK/dpalmer/${phenotype}_chr${chr}_${annotation}_test"
+
+Rscript step2_SPAtests.R \
+    --vcfFile ${vcfFile} \
+    --vcfField GT \
+    --vcfFileIndex ${vcfFileIndex} \
+    --groupFile ${groupFile} \
+    --chrom chr${chr} \
+    --minMAF 0 \
+    --maxMAFforGroupTest 0.01 \
+    --sparseSigmaFile ${sparseSigmaFile} \
+    --GMMATmodelFile ${GMMATmodelFile} \
+    --varianceRatioFile ${varianceRatioFile} \
+    --LOCO FALSE \
+    --IsSingleVarinGroupTest TRUE \
+    --cateVarRatioMinMACVecExclude "0.5,1.5,2.5,3.5,4.5,5.5,10.5,20.5" \
+    --cateVarRatioMaxMACVecInclude "1.5,2.5,3.5,4.5,5.5,10.5,20.5" \
+    --SAIGEOutputFile ${outputfile} \
+    --IsSparse TRUE \
+    --IsOutputAFinCaseCtrl TRUE \
+    --IsOutputHetHomCountsinCaseCtrl TRUE \
+    --IsOutputNinCaseCtrl TRUE \
+    --IsOutputlogPforSingle TRUE \
+    --IsSingleVarinGroupTest TRUE
